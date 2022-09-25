996fc468b88abd511bc6f604abfb8c534a0aa9db
Charly Martin <charly@hey.com>
2022-09-25 16:22:31 +0100

    Add dropdown

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import { Flex, Text, Box, Button } from "@chakra-ui/react";
-import { ethers } from "ethers";
+import { Flex, Text, Box, Button, Select } from "@chakra-ui/react";
 
 import { useWeb3React } from "@web3-react/core";
 import { injected } from "../../chain/web3-connectors";
@@ -10,14 +9,34 @@ type Props = {
   children: React.ReactNode;
 };
 
+const options = [
+  {
+    value: "Ethereum",
+    label: "Ethereum",
+    color: "#1B1BFF",
+  },
+  {
+    value: "Polygon",
+    label: "Polygon",
+    color: "#7B3FE4",
+  },
+  {
+    value: "Optimism",
+    label: "Optimism",
+    color: "#FF0420",
+  },
+];
+
 export default function Layout(props: Props) {
   const { children } = props;
   const { account, activate, error } = useWeb3React();
+  const [chain, setChain] = React.useState(options[0]);
+
   console.log("account", account, error);
 
   const connectHandler = () => {
-    activate(injected)
-  }
+    activate(injected);
+  };
 
   return (
     <Box minH="100vh" bg="gray.50" pb={32}>
@@ -51,7 +70,7 @@ export default function Layout(props: Props) {
               </Button>
             </Link>
             <Flex
-              w="200px"
+              w="260px"
               bg="white"
               borderColor="gray.200"
               borderWidth={1}
@@ -62,8 +81,27 @@ export default function Layout(props: Props) {
               justify="space-between"
             >
               <Flex align="center" justify="space-between">
-                <Box h="16px" w="16px" rounded="full" bg="blue.500" />
-                <Text ml={2}>ETH</Text>
+                <Box h="16px" w="16px" rounded="full" bg={chain.color} />
+                <Select
+                  variant="unstyled"
+                  size="sm"
+                  value={chain.value}
+                  onChange={(e) => {
+                    const chain = findSelected(e.target.value);
+                    setChain(chain || options[0]);
+                  }}
+                  ml={2}
+                  mb="1px"
+                  width="100px"
+                >
+                  {options.map((opt) => {
+                    return (
+                      <Box as="option" key={opt.value} value={opt.value}>
+                        {opt.label}
+                      </Box>
+                    );
+                  })}
+                </Select>
               </Flex>
 
               <Text>
@@ -91,4 +129,10 @@ export default function Layout(props: Props) {
       </Box>
     </Box>
   );
+
+  function findSelected(chain: string) {
+    return options.find((opt) => {
+      return opt.value == chain;
+    });
+  }
 }
